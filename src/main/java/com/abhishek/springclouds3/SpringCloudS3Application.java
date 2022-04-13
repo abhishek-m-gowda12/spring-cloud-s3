191ecd13e966248d5134a1460c8477b7e25aecd3
Abhishekgowda <abhishek@emproto.com>
2022-04-13 21:33:49 +0530

    initial commit

---
+++ src/main/java/com/abhishek/springclouds3/SpringCloudS3Application.java
@@ -0,0 +1,13 @@
+package com.abhishek.springclouds3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudS3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudS3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/abhishek/springclouds3/SpringCloudS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishek.springclouds3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudS3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
